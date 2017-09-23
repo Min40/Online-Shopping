50dccf79f8e269dd3aca0c2c7ba654d8e86ace37
Min Kebebegn <minyahil40@gmail.com>
2017-09-22 21:02:55 -0400

    setup completed

---
+++ shoppingbackend/src/main/java/com/afrohitech/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.afrohitech.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
